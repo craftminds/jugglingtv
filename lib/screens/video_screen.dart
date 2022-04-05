50a7651028c00b1b62caef25107a5494a92da9cf
Artur <artwitek@gmail.com>
2022-04-05 22:19:36 +0200

    WIP trying video_player package - didn't work

--- lib/screens/video_screen.dart
+++ lib/screens/video_screen.dart
@@ -15,16 +15,25 @@ class VideoScreen extends StatefulWidget {
 
 class _VideoScreenState extends State<VideoScreen> {
   late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
 
   @override
   void initState() {
-    super.initState();
-    _controller = VideoPlayerController.network(
-        'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4')
-      ..initialize().then((_) {
-        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-        setState(() {});
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        var vidId = ModalRoute.of(context)?.settings.arguments as int;
+        _controller = VideoPlayerController.network(
+            Provider.of<Videos>(context, listen: false)
+                .readVideoById(vidId)
+                .videoUrl);
+        _initializeVideoPlayerFuture = _controller.initialize();
       });
+    });
+    super.initState();
+  }
+
+  void dispose() {
+    _controller.dispose();
   }
 
   @override
@@ -52,27 +61,48 @@ class _VideoScreenState extends State<VideoScreen> {
           child: SingleChildScrollView(
         child: Column(children: [
           Container(
-            height: 300,
+            height: 250,
             width: double.infinity,
-            child: _controller.value.isInitialized
-                ? AspectRatio(
+            child: FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the video.
+                  return AspectRatio(
                     aspectRatio: _controller.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
                     child: VideoPlayer(_controller),
-                  )
-                : Container(),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
           ),
           FloatingActionButton(
             onPressed: () {
+              // Wrap the play or pause in a call to `setState`. This ensures the
+              // correct icon is shown.
               setState(() {
-                _controller.value.isPlaying
-                    ? _controller.pause()
-                    : _controller.play();
+                // If the video is playing, pause it.
+                if (_controller.value.isPlaying) {
+                  _controller.pause();
+                } else {
+                  // If the video is paused, play it.
+                  _controller.play();
+                }
               });
             },
+            // Display the correct icon depending on the state of the player.
             child: Icon(
               _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
             ),
-          ),
+          )
         ]),
       )),
     );
