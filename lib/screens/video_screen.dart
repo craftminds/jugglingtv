f7a508e1285820c4b5aa92c9755e1c0c65cb1686
Artur <artwitek@gmail.com>
2022-04-06 19:29:11 +0200

    Basic video player

--- lib/screens/video_screen.dart
+++ lib/screens/video_screen.dart
@@ -4,37 +4,11 @@ import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
 
 import '../providers/videos.dart';
+import '../widgets/video_item.dart';
 
-class VideoScreen extends StatefulWidget {
+class VideoScreen extends StatelessWidget {
   const VideoScreen({Key? key}) : super(key: key);
   static const routeName = '/video';
-
-  @override
-  State<VideoScreen> createState() => _VideoScreenState();
-}
-
-class _VideoScreenState extends State<VideoScreen> {
-  late VideoPlayerController _controller;
-  late Future<void> _initializeVideoPlayerFuture;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        var vidId = ModalRoute.of(context)?.settings.arguments as int;
-        _controller = VideoPlayerController.network(
-            Provider.of<Videos>(context, listen: false)
-                .readVideoById(vidId)
-                .videoUrl);
-        _initializeVideoPlayerFuture = _controller.initialize();
-      });
-    });
-    super.initState();
-  }
-
-  void dispose() {
-    _controller.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +16,9 @@ class _VideoScreenState extends State<VideoScreen> {
     final loadedvideo =
         Provider.of<Videos>(context, listen: false).readVideoById(videoId);
     final title = loadedvideo.title;
-    print(videoId);
+    final videoUrl = loadedvideo.videoUrl;
+    print(videoUrl);
+    //print(videoId);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -58,53 +34,23 @@ class _VideoScreenState extends State<VideoScreen> {
         ),
       ),
       body: Container(
-          child: SingleChildScrollView(
-        child: Column(children: [
-          Container(
-            height: 250,
-            width: double.infinity,
-            child: FutureBuilder(
-              future: _initializeVideoPlayerFuture,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  // If the VideoPlayerController has finished initialization, use
-                  // the data it provides to limit the aspect ratio of the video.
-                  return AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    // Use the VideoPlayer widget to display the video.
-                    child: VideoPlayer(_controller),
-                  );
-                } else {
-                  // If the VideoPlayerController is still initializing, show a
-                  // loading spinner.
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 270,
+                width: double.infinity,
+                child: VideoItem(
+                  videoPlayerController:
+                      VideoPlayerController.network(videoUrl),
+                  autoplay: false,
+                  looping: false,
+                ),
+              )
+            ],
           ),
-          FloatingActionButton(
-            onPressed: () {
-              // Wrap the play or pause in a call to `setState`. This ensures the
-              // correct icon is shown.
-              setState(() {
-                // If the video is playing, pause it.
-                if (_controller.value.isPlaying) {
-                  _controller.pause();
-                } else {
-                  // If the video is paused, play it.
-                  _controller.play();
-                }
-              });
-            },
-            // Display the correct icon depending on the state of the player.
-            child: Icon(
-              _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-            ),
-          )
-        ]),
-      )),
+        ),
+      ),
     );
   }
 }
