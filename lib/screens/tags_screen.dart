061000e38b60528d6f07bd220fa49e612dffdc5d
Artur <artwitek@gmail.com>
2022-04-24 00:42:02 +0200

    Tags screen and filtering works

--- lib/screens/tags_screen.dart
+++ lib/screens/tags_screen.dart
@@ -30,66 +30,71 @@ class _TagsScreenState extends State<TagsScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          TextField(
-            onChanged: (value) {
-              setState(() {
-                searchString = value;
-              });
-            },
-            //controller: searchController,
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.search),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  searchString = value;
+                });
+              },
+              //controller: searchController,
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Container(
-              constraints: const BoxConstraints(maxHeight: double.infinity),
-              width: MediaQuery.of(context).size.width,
-              child: FutureBuilder<List<Tag>>(
-                  future: Provider.of<Tags>(context).fetchAndSetTags(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Center(
-                        child: Text("${snapshot.error}"),
-                      );
-                    } else if (snapshot.hasData) {
-                      return Wrap(
-                        runAlignment: WrapAlignment.start,
-                        direction: Axis.horizontal,
-                        spacing: 20.0,
-                        children: <Widget>[
-                          for (var item in snapshot.data!)
-                            Text(
-                              item.name,
-                              style: TextStyle(
-                                  //fontWeight: FontWeight.w600,
-                                  //fontSize: 20,
-                                  height: 3,
-                                  background: Paint()
-                                    ..strokeWidth = 18.0
-                                    ..color =
-                                        const Color.fromARGB(255, 255, 186, 8)
-                                    ..style = PaintingStyle.stroke
-                                    ..strokeJoin = StrokeJoin.round),
-                            ),
-                        ],
-                        //
-                      );
-                    } else {
-                      return const Center(
-                        child: CircularProgressIndicator(
-                          color: Colors.grey,
-                          strokeWidth: 1.0,
-                        ),
-                      );
-                    }
-                  }),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                constraints: const BoxConstraints(maxHeight: double.infinity),
+                width: MediaQuery.of(context).size.width,
+                child: FutureBuilder<List<Tag>>(
+                    future: Provider.of<Tags>(context).fetchAndSetTags(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return Center(
+                          child: Text("${snapshot.error}"),
+                        );
+                      } else if (snapshot.hasData) {
+                        return Wrap(
+                          runAlignment: WrapAlignment.start,
+                          direction: Axis.horizontal,
+                          spacing: 20.0,
+                          children: <Widget>[
+                            for (var item in snapshot.data!.where((tag) => tag
+                                .name
+                                .toLowerCase()
+                                .contains(searchString.toLowerCase())))
+                              Text(
+                                item.name,
+                                style: TextStyle(
+                                    //fontWeight: FontWeight.w600,
+                                    //fontSize: 20,
+                                    height: 3,
+                                    background: Paint()
+                                      ..strokeWidth = 18.0
+                                      ..color =
+                                          const Color.fromARGB(255, 255, 186, 8)
+                                      ..style = PaintingStyle.stroke
+                                      ..strokeJoin = StrokeJoin.round),
+                              )
+                          ],
+                          //
+                        );
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.grey,
+                            strokeWidth: 1.0,
+                          ),
+                        );
+                      }
+                    }),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
