1c3463d6b945e415d80ac9aa08c296d7e2f77b55
Artur <artwitek@gmail.com>
2022-04-25 23:25:03 +0200

    WIP filtering works, lost when textfield open

--- lib/screens/tags_screen.dart
+++ lib/screens/tags_screen.dart
@@ -1,112 +1,119 @@
+import 'package:filter_list/filter_list.dart';
 import 'package:flutter/material.dart';
 import '../providers/tags.dart';
 import '../models/videos_db.dart';
 import 'package:provider/provider.dart';
 
-class TagsScreen extends StatefulWidget {
+class TagsScreen extends StatelessWidget {
   static const routeName = '/tags-screen';
   const TagsScreen({Key? key}) : super(key: key);
 
   @override
-  State<TagsScreen> createState() => _TagsScreenState();
-}
-
-class _TagsScreenState extends State<TagsScreen> {
-  String searchString = '';
-
-  @override
   Widget build(BuildContext context) {
+    List<Tag> selectedTagList = [];
+    //List<Tag> tagList;
+    AppBar appBar = AppBar(
+      backgroundColor: Colors.white,
+      iconTheme: const IconThemeData(
+        color: Colors.black54,
+      ),
+      title: Center(
+        child: Image.asset(
+          "assets/images/logo.png",
+          //fit: BoxFit.cover,
+          scale: 1.5,
+        ),
+      ),
+    );
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(
-          color: Colors.black54,
-        ),
-        title: Center(
-          child: Image.asset(
-            "assets/images/logo.png",
-            //fit: BoxFit.cover,
-            scale: 1.5,
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.cancel_presentation_sharp),
-              label: 'Clear Selection'),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-        ],
-        selectedItemColor: Colors.grey,
-        unselectedItemColor: Colors.grey,
-      ),
+      appBar: appBar,
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const <BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(
+      //         icon: Icon(Icons.cancel_presentation_sharp),
+      //         label: 'Clear Selection'),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.search),
+      //       label: 'Search',
+      //     ),
+      //   ],
+      //   selectedItemColor: Colors.grey,
+      //   unselectedItemColor: Colors.grey,
+      // ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  searchString = value;
-                });
-              },
-              //controller: searchController,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.search),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                constraints: const BoxConstraints(maxHeight: double.infinity),
-                width: MediaQuery.of(context).size.width,
-                child: FutureBuilder<List<Tag>>(
-                    future: Provider.of<Tags>(context).fetchAndSetTags(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasError) {
-                        return Center(
-                          child: Text("${snapshot.error}"),
-                        );
-                      } else if (snapshot.hasData) {
-                        return Wrap(
-                          runAlignment: WrapAlignment.start,
-                          direction: Axis.horizontal,
-                          spacing: 20.0,
-                          children: <Widget>[
-                            for (var item in snapshot.data!.where((tag) => tag
-                                .name
-                                .toLowerCase()
-                                .contains(searchString.toLowerCase())))
-                              Text(
-                                item.name,
-                                style: TextStyle(
-                                    //fontWeight: FontWeight.w600,
-                                    //fontSize: 20,
-                                    height: 3,
-                                    background: Paint()
-                                      ..strokeWidth = 18.0
-                                      ..color =
-                                          const Color.fromARGB(255, 255, 186, 8)
-                                      ..style = PaintingStyle.stroke
-                                      ..strokeJoin = StrokeJoin.round),
-                              )
-                          ],
-                          //
-                        );
-                      } else {
-                        return const Center(
-                          child: CircularProgressIndicator(
-                            color: Colors.grey,
-                            strokeWidth: 1.0,
-                          ),
-                        );
-                      }
-                    }),
-              ),
-            ),
-          ],
+        child:
+            // Column(
+            //   children: [
+            //     TextField(
+            //       onChanged: (value) {
+            //         setState(() {
+            //           searchString = value;
+            //         });
+            //       },
+            //       //controller: searchController,
+            //       decoration: const InputDecoration(
+            //         prefixIcon: Icon(Icons.search),
+            //       ),
+            //     ),
+            Container(
+          height:
+              MediaQuery.of(context).size.height - appBar.preferredSize.height,
+          width: MediaQuery.of(context).size.width,
+          child: FutureBuilder<List<Tag>>(
+              future: Provider.of<Tags>(context).fetchAndSetTags(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("${snapshot.error}"),
+                  );
+                } else if (snapshot.hasData) {
+                  //tagList = snapshot.data as List<Tag>;
+                  return Container(
+                    constraints:
+                        const BoxConstraints(maxHeight: double.infinity),
+                    width: MediaQuery.of(context).size.width,
+                    child: FilterListWidget<Tag>(
+                      listData: snapshot.data,
+                      themeData: FilterListThemeData(context,
+                          backgroundColor:
+                              Theme.of(context).scaffoldBackgroundColor,
+                          headerTheme: HeaderThemeData(
+                              backgroundColor:
+                                  Theme.of(context).scaffoldBackgroundColor,
+                              searchFieldBackgroundColor: Colors.black12)),
+                      selectedListData: selectedTagList,
+                      validateSelectedItem: (list, val) {
+                        //selectedTagList = list as List<Tag>;
+
+                        ///  identify if item is selected or not
+                        return list!.contains(val);
+                      },
+                      choiceChipLabel: (item) {
+                        return item!.name;
+                      },
+                      onItemSearch: (tag, query) {
+                        return tag.name
+                            .toLowerCase()
+                            .contains(query.toLowerCase());
+                      },
+                      onApplyButtonClick: (list) {
+                        //selectedTagList = list as List<Tag>;
+                        //TODO: it must first ask the database and then open new page with the list of movies. Filtered tags must shown when clicked maybe in the right top corner
+                        print(selectedTagList);
+
+                        //Navigator.pop(context, list);
+                      },
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.grey,
+                      strokeWidth: 1.0,
+                    ),
+                  );
+                }
+              }),
         ),
       ),
     );
