bba1d5ad943e13fae07a5ccd6f4fcab5a96e3114
Artur <artwitek@gmail.com>
2022-07-02 11:05:00 +0200

    Added the author image url to video info (video url previously)

--- lib/widgets/video_info.dart
+++ lib/widgets/video_info.dart
@@ -97,7 +97,7 @@ class VideoInfo extends StatelessWidget {
             children: [
               CircleAvatar(
                 //TODO: change for the author avatar - scrape it from the page for every author
-                foregroundImage: NetworkImage(loadedvideo.thumbnailUrl),
+                foregroundImage: NetworkImage(loadedvideo.authorImageUrl),
               ),
               const SizedBox(width: 10.0),
               Column(
