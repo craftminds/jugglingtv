7e5d519af2096c5c388a169414dbc5a0547a7671
Artur <artwitek@gmail.com>
2023-05-04 23:34:27 +0200

    Update for iOS 1.1.3 upload. Mainly xcode automatic updates.

---
+++ android/app/src/main/kotlin/com/ubicircus/jugglingtv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ubicircus.jugglingtv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -25,9 +25,7 @@ Hello!
 
 This app is the copy of well known website juggling.tv. It was created by permission of the owners of the website.
 
-The app is not based on the original database, as everything piece of information was webscraped by mine, I never asked for the original data.
-
-It is run in archive mode and you cannot login nor add any videos. Not every data was scraped thus, I didn\'t try to scrape the comments or more relations and it was getting more and more complicated for me, and for the first app ever written the amount of information to be learned was really huge.
+It is running in archive mode and you cannot login nor add any videos (hence you can like and share - liked videos are saved only on your device).
 
 Enjoy the moment of flashback!                                
 ''',
